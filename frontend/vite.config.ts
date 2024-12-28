e08eecece51faa0164092e3bdf9fc79eaeb91c69
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2024-12-28 20:17:41 +0530

    dockerize the application

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+   port: 5173,
+   strictPort: true,
+  },
+  server: {
+   port: 5173,
+   strictPort: true,
+   host: '0.0.0.0',
+  },
+ });
